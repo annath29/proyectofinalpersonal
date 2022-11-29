8fa1934d67836c8baf0be652fe71c9db9216dc47
407 <407@academicos.cesde.edu.co>
2022-11-29 16:37:35 -0500

    se inica proyecto final

---
+++ src/Home/Home.js
@@ -0,0 +1,77 @@
+import './Home.css'
+export function Home(){
+    return(
+        <>
+        <section>
+            <div class="banner">
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-8">
+                        <h2>Arctic Monkeys</h2>
+                        <p> Arctic Monkeys es una banda británica de rock, formada en Sheffield, Reino Unido.3​ El grupo está compuesto por el guitarrista principal y vocalista Alex Turner, el guitarrista Jamie Cook, el baterista Matt Helders y el bajista Nick O'Malley. El bajista original de la banda, Andy Nicholson, dejó el proyecto en 2006 poco después del lanzamiento del álbum debut.<br></br>Los Arctic Monkeys fueron considerados como una de las primeras bandas en hacerse conocidas al público gracias a la difusión de sus canciones por Internet, por lo cual se los catalogó como «la banda que pudo haber cambiado el mercado de la música».4​ Esto le valió para que su álbum de estudio debut, Whatever People Say I Am, That's What I'm Not (2006), se convierta en el disco debut británico más vendido en la historia, con más de 300 mil copias vendidas en su primera semana en el mercado.5​ Ganó el premio al Mejor Álbum Británico en los Brit Awards 2007. El segundo álbum de la banda, Favourite Worst Nightmare (2007), contó casi con el mismo éxito, ganando también el premio al Mejor Álbum Británico en los Brit Awards 2008. Luego pasarían a sacar Humbug (2009), un proyecto más experimental, y Suck It and See (2011).
+                        </p>
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyang-a7d3b.appspot.com/o/concierto.jpg?alt=media&token=9854152a-ed5d-44c8-8ca5-3e50bd6fa148" class="img-fluid w-100"></img>
+                    </div>
+                    <div class="col-12 col-md-4 mt-3">
+                        <h2 class="text-center mb-3">Conciertos</h2>
+                        <div class="row">
+                            <div class="col-6 text-end">
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            </div>
+                            <div class="col">
+                                <h3>Medellin</h3>
+                                <h5>Dec 2</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 text-end">
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            </div>
+                            <div class="col">
+                                <h3>Cali</h3>
+                                <h5>Dec 3</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 text-end">
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            </div>
+                            <div class="col">
+                                <h3>Amsterdam</h3>
+                                <h5>Dec 15</h5>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6 text-end">
+                            <i class="bi bi-vinyl-fill fs-1"></i>
+                            </div>
+                            <div class="col">
+                                <h3>Sidny</h3>
+                                <h5>Dec 30</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyang-a7d3b.appspot.com/o/tour.jpg?alt=media&token=8223b9f5-6234-4bf6-a7c5-d5b170467fa8" alt="" class="img-fluid w-100 h-50"/>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyang-a7d3b.appspot.com/o/conciertbog.png?alt=media&token=c1ecc1cd-655f-43e3-8e1d-9f8d1210099b" alt="" class="img-fluid w-100 h-50"/>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <img src="https://firebasestorage.googleapis.com/v0/b/spotifyang-a7d3b.appspot.com/o/arctic-nuevo-album.jpg?alt=media&token=ad5c4773-3a93-481a-bf9a-803ac4351577" alt="" class="img-fluid w-100 h-50"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
